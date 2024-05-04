f2adb7e109bf14a4051358b0d0b2fc9f252d3246
DanielLieblen <plocks.me@gmail.com>
2024-05-03 23:54:49 -0300

    comitando readme e desafio.js

---
+++ Desafio2.js
@@ -0,0 +1,32 @@
+
+
+
+
+let quantidade = parseInt(gets());
+
+
+
+for(let i = 0; i < quantidade; i++){
+    let nomeHeroi = gets();
+    let experiencia = parseInt(gets());
+
+    if(experiencia <= 1000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel ferro")
+    }else if(experiencia > 1000 && experiencia <= 2000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel bronze")
+    }else if(experiencia > 2000 && experiencia <= 5000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel prata")
+    }else if(experiencia > 5000 && experiencia <= 7000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel ouro")
+    }else if(experiencia > 7000 && experiencia <= 9000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel platina")
+    }else if(experiencia > 9000 && experiencia <= 11000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel diamante")
+    }else if(experiencia > 11000 && experiencia <= 13000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel ascendente")
+    }else if(experiencia > 13000 && experiencia <= 15000){
+        console.log("o heroi de nome" + nomeHeroi + "esta no nivel immortal")
+    }
+
+
+}
